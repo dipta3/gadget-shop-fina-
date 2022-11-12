252aaa0068d6caf7318a882fabc14e458e8fe20a
Dipta Biswas <diptabiswas79@gmail.com>
2022-11-12 20:20:02 +0600

    cart added

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -32,6 +32,8 @@ import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
 import { contextStore } from "../utils/Store";
 import dynamic from "next/dynamic";
+import Gohome from "./Gohome";
+
 function Cart() {
   const [deliveryCharcg, setDeliveryCharge] = useState(0);
   const { state, dispatch } = useContext(contextStore);
@@ -52,9 +54,10 @@ function Cart() {
   if (products.length < 1) {
     return (
       <Container sx={{ marginTop: "25px" }}>
+        
         <Alert severity="info">
           <AlertTitle>Info</AlertTitle>
-          Sorry, your cart is empty. Please,
+          Sorry, Your Cart Is Empty. Please, 
           <strong>
             <Link href={"/"} passHref>
               <a> Go to shopping</a>
@@ -66,6 +69,7 @@ function Cart() {
   }
   return (
     <Container>
+      <Gohome></Gohome>
       <Grid container spacing={2} my={5}>
         <Grid item sx={{ width: { xs: "100%", md: "70%" } }}>
           <Paper variant="outlined">
@@ -73,10 +77,10 @@ function Cart() {
               <Table>
                 <TableHead sx={{ backgroundColor: "#ECECEE" }}>
                   <TableRow>
-                    <TableCell>Img</TableCell>
-                    <TableCell>Name</TableCell>
+                    <TableCell>Product Image</TableCell>
+                    <TableCell>Product Name</TableCell>
                     <TableCell>Price</TableCell>
-                    <TableCell>Qty</TableCell>
+                    <TableCell>Quantity</TableCell>
                     <TableCell>Remove</TableCell>
                   </TableRow>
                 </TableHead>
@@ -93,8 +97,10 @@ function Cart() {
                               quantity={100}
                             />
                           </TableCell>
-                          <TableCell>{product.name}</TableCell>
-                          <TableCell>{product.price}</TableCell>
+                          <TableCell><b>{product.name}</b></TableCell>
+                          <TableCell
+                          sx={{color:'#f85606'}}
+                          ><b>৳{product.price} </b> </TableCell>
                           <TableCell>
                             <IconButton
                               onClick={() =>
@@ -145,8 +151,7 @@ function Cart() {
             <List dense={true}>
               <ListItem>
                 <ListItemText>
-                  <strong>Price:</strong>{" "}
-                  {products.reduce((a, c) => a + c.price * c.qty, 0)}
+                  Subtotal:<Typography  sx={{color:'#f85606'}}><b>৳{products.reduce((a, c) => a + c.price * c.qty, 0)}</b></Typography> 
                 </ListItemText>
               </ListItem>
               <ListItem>
@@ -169,7 +174,7 @@ function Cart() {
                       <MenuItem value={60}>Mirpur(60)</MenuItem>
                       <MenuItem value={70}>New Market(70)</MenuItem>
                       <MenuItem value={50}>Rampura(50)</MenuItem>
-                      <MenuItem value={70}>Out side of dhaka(100)</MenuItem>
+                      <MenuItem value={100}>Out Side Dhaka(100)</MenuItem>
                     </Select>
                   </FormControl>
                 </ListItemText>
@@ -177,21 +182,21 @@ function Cart() {
 
               <Divider></Divider>
               <ListItem>
-                <ListItemText>
-                  <strong>Total:</strong>{" "}
+                <ListItemText sx={{color:'#f85606'}}>
+                  Total:{" "}<b>৳
                   {products.reduce((a, c) => a + c.price * c.qty, 0) +
-                    deliveryCharcg}
+                    deliveryCharcg}</b>
                 </ListItemText>
               </ListItem>
               <ListItem>
                 <Button
                   disabled={deliveryCharcg ? false : true}
-                  sx={{ background: "#FFDA00", color: "#000000" }}
+                  sx={{ background: "#AFE1AF", color: "#03B0D6" }}
                   fullWidth
                   variant="outlined"
                   onClick={cartToCheckout}
                 >
-                  Checkout
+                  Check out
                 </Button>
               </ListItem>
             </List>
